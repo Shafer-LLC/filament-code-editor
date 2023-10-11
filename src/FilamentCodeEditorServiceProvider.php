a59eef274abc636044137c7b83ac0cd034c60bc0
Tom Shafer <tj@tjshafer.com>
2023-10-11 13:59:49 -0700

    Update FilamentCodeEditorServiceProvider.php

--- src/FilamentCodeEditorServiceProvider.php
+++ src/FilamentCodeEditorServiceProvider.php
@@ -2,25 +2,14 @@
 
 namespace SebastiaanKloos\FilamentCodeEditor;
 
-use Filament\Facades\Filament;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class FilamentCodeEditorServiceProvider extends PackageServiceProvider
 {
-    protected array $beforeCoreScripts = [
-        'filament-tools' => __DIR__.'/../dist/filament-tools.js',
-    ];
 
     public function configurePackage(Package $package): void
     {
         $package->name('filament-code-editor')->hasViews();
-    }
-
-    public function bootingPackage()
-    {
-        Filament::serving(function () {
-            Filament::registerScripts($this->beforeCoreScripts, true);
-        });
     }
 }
