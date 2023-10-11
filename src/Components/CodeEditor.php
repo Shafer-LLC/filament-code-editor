40048832e923eb001e109b8668da78d9c27c9eee
Tom Shafer <tj@tjshafer.com>
2023-10-11 14:00:11 -0700

    Update CodeEditor.php

--- src/Components/CodeEditor.php
+++ src/Components/CodeEditor.php
@@ -4,14 +4,14 @@ namespace SebastiaanKloos\FilamentCodeEditor\Components;
 
 use Filament\Forms\Components\Field;
 use Filament\Forms\Components\Contracts\CanBeLengthConstrained;
-class CodeEditor extends Field implements Contracts\CanBeLengthConstrained
+class CodeEditor extends Field implements CanBeLengthConstrained
 {
-    use Filament\Forms\Components\Concerns\CanBeAutocompleted;
-    use Filament\Forms\Components\Concerns\CanBeLengthConstrained;
-    use Filament\Forms\Components\Concerns\CanBeReadOnly;
-    use Filament\Forms\Components\Concerns\HasExtraInputAttributes;
-    use Filament\Forms\Components\Concerns\HasPlaceholder;
-    use Filament\Forms\Components\HasExtraAlpineAttributes;
+    use \Filament\Forms\Components\Concerns\CanBeAutocompleted;
+    use \Filament\Forms\Components\Concerns\CanBeLengthConstrained;
+    use \Filament\Forms\Components\Concerns\CanBeReadOnly;
+    use \Filament\Forms\Components\Concerns\HasExtraInputAttributes;
+    use \Filament\Forms\Components\Concerns\HasPlaceholder;
+    use \Filament\Forms\Components\HasExtraAlpineAttributes;
     
     protected string $view = 'filament-code-editor::components.code-editor';
 }
